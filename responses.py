d6d7e848f0af3353b42087f9f521b4cf17156ac1
forrest <indubitablyforrest@gmail.com>
2023-08-02 08:10:45 -0500

    a little cleanup to get rid of warnings and other IDE complaints

--- responses.py
+++ responses.py
@@ -1,11 +1,11 @@
-import cascade
+import cascadefun
 
 
 def handle_response(message) -> str:
     p_message = message.lower()
 
     if p_message == '/cascade':
-        return cascade.cascade_resolved()
+        return cascadefun.cascade_resolved()
 
     if p_message == '/help' | '/commands':
         return 'here is what i can do: /cascade, /help'
